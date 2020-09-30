e2e8c2c67f96b3fbb65a42aff069537b6dd8d246
AnneSMAL <AnneSMAL@users.noreply.github.com>
2020-09-30 16:51:10 +0200

    game_state

--- projetIA/game/urls.py
+++ projetIA/game/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('move', views.apply_move)
 ]
--- projetIA/game/views.py
+++ projetIA/game/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the game index.")
+
+    game_state = {
+        "game_id" : 11,
+        "board" : [[1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,2]],
+        "players" : [{
+                "id" :  10,
+                "name" : "Alice",
+                "color" : "cyan",
+                "position" : [0,0]
+            },{
+                "id" :  20,
+                "name" : "Bob",
+                "color" : "orange",
+                "position" : [7,7]
+            }],
+        "current_player" : 1,
+        "code" : 0
+    }
+
+    return HttpResponse(json.dumps(game_state))
+
+def apply_move(request) :
+
+
+    game_state = {
+        "game_id" : 11,
+        "board" : [[1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,2]],
+        "players" : [{
+                "id" :  10,
+                "name" : "Alice",
+                "color" : "cyan",
+                "position" : [0,0]
+            },{
+                "id" :  20,
+                "name" : "Bob",
+                "color" : "orange",
+                "position" : [7,7]
+            }],
+        "current_player" : 1,
+        "code" : 0
+    }
+    return HttpResponse(json.dumps(game_state))
